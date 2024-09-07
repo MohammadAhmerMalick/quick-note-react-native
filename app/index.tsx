7f009019bfca2ffabc076d3e674a084599057dfd
unknown <mohammadahmermalick@gmail.com>
2024-09-07 15:25:29 +0400

    cleaned project

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const index = () => {
+  return (
+    <SafeAreaView style={{ backgroundColor: 'transparent' }}>
+      <Text>Quick Note</Text>
+    </SafeAreaView>
+  )
+}
+
+export default index
---
+++ constants.ts
@@ -0,0 +1,1 @@
+export const PriamryColor = '#EAB308'
